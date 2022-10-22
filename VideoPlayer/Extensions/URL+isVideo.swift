d92c80a3e65498948cfd42cac8e02a459b99c7db
Lev Poznyakov <levochkaaa@icloud.com>
2022-10-22 14:08:52 +0300

    don't need a isFolder check

--- VideoPlayer/Extensions/URL+isVideo.swift
+++ VideoPlayer/Extensions/URL+isVideo.swift
@@ -1,12 +1,9 @@
-// URL+isSmth.swift
+// URL+isVideo.swift
 
 import Foundation
 
 extension URL {
     var isVideo: Bool {
         return self.pathExtension == "mp4"
-    }
-    var isDirectory: Bool {
-        return !self.isFileURL
     }
 }
